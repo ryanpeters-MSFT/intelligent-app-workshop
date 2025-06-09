b1cb1abb026bace7be20211c726c3a6a2fc9e5c4
Ryan Peters <ryanpeters@microsoft.com>
2025-06-09 17:13:19 +0000

    lesson 2

--- workshop/dotnet/Lessons/Lesson2/Program.cs
+++ workshop/dotnet/Lessons/Lesson2/Program.cs
@@ -1,6 +1,7 @@
 using Core.Utilities.Config;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.ChatCompletion;
 
 // TODO: Step 1 - add ChatCompletion import
 
@@ -9,16 +10,10 @@ IKernelBuilder builder = KernelBuilderProvider.CreateKernelWithChatCompletion();
 Kernel kernel = builder.Build();
 
 // TODO: Step 2a - Get chatCompletionService and initialize chatHistory with system prompt
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+ChatHistory chatHistory = new("You are a friendly financial advisor that only emits financial advice in a creative and funny tone");
 
 // TODO: Step 2b - Remove the promptExecutionSettings and kernelArgs initialization code
-OpenAIPromptExecutionSettings promptExecutionSettings = new()
-{
-    // Add Auto invoke kernel functions as the tool call behavior
-    ChatSystemPrompt = @"You are a friendly financial advisor that only emits financial advice in a creative and funny tone"
-};
-
-// Initialize kernel arguments
-KernelArguments kernelArgs = new(promptExecutionSettings);
 
 // Execute program.
 const string terminationPhrase = "quit";
@@ -32,14 +27,17 @@ do
     {
         Console.Write("Assistant > ");
         // TODO: Step 3 - Initialize fullMessage variable and add user input to chat history
-
+        string fullMessage = "";
+        chatHistory.AddUserMessage(userInput);
 
         // TODO: Step 4 - Remove the foreach loop and replace it with `chatCompletionService` code
         // including adding assistant message to chat history
-        await foreach (var response in kernel.InvokePromptStreamingAsync(userInput, kernelArgs))
+        await foreach (var chatUpdate in chatCompletionService.GetStreamingChatMessageContentsAsync(chatHistory))
         {
-            Console.Write(response);
+            Console.Write(chatUpdate.Content);
+            fullMessage += chatUpdate.Content ?? "";
         }
+        chatHistory.AddAssistantMessage(fullMessage);
 
         Console.WriteLine();
     }
